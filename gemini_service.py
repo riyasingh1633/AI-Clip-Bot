3c906878b0bbcc36e12f9a90234a37c1b3101cb2
riyasingh1633 <riyasingh1633@gmail.com>
2026-08-07 22:43:36 +0530

    gemini_service.py

---
+++ gemini_service.py
@@ -0,0 +1,41 @@
+import os
+import json
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+def find_best_clips(transcript):
+    prompt = f"""
+You are an expert viral short-form video editor.
+
+Analyze the transcript below.
+
+Choose the BEST 5 moments that would perform well as YouTube Shorts, Instagram Reels, or TikTok.
+
+Return ONLY valid JSON.
+
+Format:
+
+[
+  {{
+    "start":"00:00:00",
+    "end":"00:00:30",
+    "reason":"Why this clip is viral"
+  }}
+]
+
+Transcript:
+
+{transcript}
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+    )
+
+    return response.text
